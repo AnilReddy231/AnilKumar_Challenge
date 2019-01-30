953167ed331b3418bfdc88dbe6aacc0742244946
AnilReddy231 <anilreddy231.gajulapalli@gmail.com>
2019-01-29 20:51:46 -0500

    Updated Args

--- CreditCard_Validator/CreditCard_Validator.py
+++ CreditCard_Validator/CreditCard_Validator.py
@@ -1,10 +1,9 @@
 import re
 
 def Validate_Card(card):
-	valid_structure = r"[456]\d{3}(-?\d{4}){3}$"
-	no_four_repeats = r"((\d)-?(?!(-?\2){3})){16}"
-	filters = valid_structure, no_four_repeats
-	if all(re.match(f, card) for f in filters):
+	valid_format = r"[456]\d{3}(-?\d{4}){3}$"
+	avoid_four_repeats = r"((\d)-?(?!(-?\2){3})){16}"
+	if all(re.match(f, card) for f in [valid_format,avoid_four_repeats]):
 		return True
 	else:
 		return False
